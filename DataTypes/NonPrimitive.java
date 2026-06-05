86dc73af039630db5d622b5f5ae950fbd1820de6
digvijay parade <digvijayparde@gmail.com>
2026-06-06 00:22:12 +0530

    added  new file

---
+++ DataTypes/NonPrimitive.java
@@ -0,0 +1,24 @@
+public class NonPrimitive{
+
+	public static void main(String[]args){
+
+		byte age = 23;
+		short year = 2026;
+		int salary = 50000;
+		long population = 1000000000;
+		float percentage = 89.5f;
+		double d = 3.555555;
+		char grade = 'A';
+		boolean isImGood = true;
+
+		 System.out.println("byte: " + age);
+        	System.out.println("short: " + year);
+        	System.out.println("int: " + salary);
+        	System.out.println("long: " + population);
+        	System.out.println("float: " + percentage);
+        	System.out.println("double: " + d);
+        	System.out.println("char: " + grade);
+        	System.out.println("boolean: " + isImGood);
+
+	}
+}
