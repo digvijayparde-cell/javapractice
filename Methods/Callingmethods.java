425a75cb990888ea5a585472303e35fc19aead70
digvijay parade <digvijayparde@gmail.com>
2026-06-12 23:06:38 +0530

    more practice

---
+++ Methods/Callingmethods.java
@@ -0,0 +1,17 @@
+public class Callingmethods{
+
+	public static void main(String[]args){
+
+		New obj = new New();
+		obj.add(20.10,1000000L);
+
+		System.out.println(New.sub(2000,1000));
+
+		Simple.schname("Zp HS");
+		Simple s = new Simple();
+
+		s.id(101);
+
+	}
+
+}
