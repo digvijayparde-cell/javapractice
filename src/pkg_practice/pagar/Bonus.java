1c0536fa964b7fb1a3a5c5ed4ecfd20058d94d5c
digvijay parade <digvijayparde@gmail.com>
2026-06-22 16:44:59 +0530

    /* accessing a method from different packages class which have same 		name as this class*/

---
+++ src/pkg_practice/pagar/Bonus.java
@@ -0,0 +1,15 @@
+package pkg_practice.pagar;
+
+public class Bonus {
+
+	public static void main(String[]args) {
+		
+		/* accessing a method from different packages class which have same
+		name as this class*/
+		pkg_testing.salary.Bonus.calculate(10000000);
+		
+	}
+	
+	
+	
+}
