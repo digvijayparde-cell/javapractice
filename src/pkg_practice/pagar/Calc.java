ce17dae7f6098fb04eac0884774c7428106fd0fc
digvijay parade <digvijayparde@gmail.com>
2026-06-22 08:14:06 +0530

    importing sub-package into another sub-package

---
+++ src/pkg_practice/pagar/Calc.java
@@ -0,0 +1,12 @@
+package pkg_practice.pagar;
+import pkg_testing.salary.Bonus;
+public class Calc {
+
+	public static void main(String[]args) {
+		
+		// importing sub-package into another sub-package
+		Bonus.calculate(100000);
+		
+	}
+	
+}
