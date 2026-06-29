1582685abcc175ec248c5e2c80b553dcef113cc0
digvijay parade <digvijayparde@gmail.com>
2026-06-29 11:57:03 +0530

    loop

---
+++ src/controlflow/CombinedExample.java
@@ -0,0 +1,23 @@
+package controlflow;
+
+public class CombinedExample {
+
+	public static void main(String[] args) {
+		
+		long startTime = System.nanoTime();
+		
+		int i;
+		for (i = 1; i <= 5; i++) {
+			System.out.print(i + " ");
+		}
+		
+		System.out.println();
+		
+		long endTime = System.nanoTime();
+		long duration = endTime - startTime;
+		
+		System.out.println("OS: " + System.getProperty("os.name"));
+		System.out.println("Loop variable final value: " + i);
+		System.out.println("Time taken (nanoseconds): " + duration);
+	}
+}
---
+++ src/controlflow/ForLoop.java
@@ -0,0 +1,17 @@
+package controlflow;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//i <= 10 only if his condition is true than jvm will execute  i++ this
+		
+		int i ;
+		for(i = 11; i >= 10 ; i++ ) {
+		
+		System.out.println(i+"");
+
+	}
+
+}
+}
