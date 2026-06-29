5c56e0cdb25207187b4a33a90a1c1d9a6adac34b
digvijay parade <digvijayparde@gmail.com>
2026-06-29 12:15:40 +0530

    .

---
+++ src/controlflow/Demo.java
@@ -0,0 +1,23 @@
+package controlflow;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i;
+		
+		for(i = 1 ; i <= 5 ; i++) {
+			
+			vijay();
+			
+		}
+
+	}
+	
+	public static void  vijay() {
+		
+		System.out.println("dumb vijay");
+	}
+
+}
