8051910f2582844a1ed7ae09c0498225e19f7156
digvijay parade <digvijayparde@gmail.com>
2026-06-30 23:10:15 +0530

    .

--- src/controlflow/JobApplication.java
+++ src/controlflow/JobApplication.java
@@ -1,43 +1,27 @@
 package controlflow;
 
 public class JobApplication {
-	
-	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		JobApplication cand1 = new JobApplication();
-		cand1.application(100, 200, 31, "Female");
-		
-		
-	
-	}
-	
-	public void application(double percentage,int experience,int age,String gender) {
-		
-if(percentage == 99.99 && experience > 6 && (age <= 30 || gender == "Female")) {
-			
-			selected();
-			
-		}
+    public static void main(String[] args) {
+        JobApplication cand1 = new JobApplication();
+        
+        cand1.application("cand1", 100, 200, 31, "Female");
+    }
 
-else {
-	
-	rejected();
-}
+    public void application(String name, double percentage, int experience, int age, String gender) {
+       
+        if (percentage == 99.99 && experience > 6 && (age <= 30 || gender.equals("Female"))) {
+            selected(name);
+        } else {
+            rejected(name);
+        }
+    }
 
-}
-	
-	public void selected() {
-		
-		System.out.println("Congrulation your selected");
-		
-	}
-	
-	public void rejected() {
-		
-		System.out.println("your rejected , better luch next time");
-		
-	}
+    public void selected(String name) {
+        System.out.println(name + " Congratulations, you are selected!");
+    }
+
+    public void rejected(String name) {
+        System.out.println(name + " you are rejected, better luck next time.");
+    }
 }
