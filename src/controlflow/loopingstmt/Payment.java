dda029b1697641cbb8aae4dceaca1f46911cd7c6
digvijay parade <digvijayparde@gmail.com>
2026-07-06 22:30:08 +0530

    ,,

---
+++ src/controlflow/loopingstmt/Payment.java
@@ -0,0 +1,16 @@
+package controlflow.loopingstmt;
+
+public class Payment {
+
+	public static void main(String[] args) {
+		
+		int[] salaries = {30000, 45000, 50000, 28000, 60000};
+		
+		for (int i = 0; i < salaries.length; i++) {
+		
+			double newsalary = salaries[i] + (salaries[i] * 0.10);
+			
+			System.out.println("Emp " + (i + 1) + newsalary);
+		}
+	}
+}
