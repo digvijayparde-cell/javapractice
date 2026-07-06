ace9d850eede43b89f9d8da73583ad7be8177f91
digvijay parade <digvijayparde@gmail.com>
2026-07-07 00:20:43 +0530

    ;

--- src/controlflow/loopingstmt/Tables.java
+++ src/controlflow/loopingstmt/Tables.java
@@ -5,15 +5,17 @@ public class Tables{
 	public static void main(String[]args) {
 		
 		Tables t1 = new Tables();
-		t1.tableOf(8);;
+		t1.tableOf(4000000);;
 		
 	}
 		
 public void tableOf(int num) {
 	
+	System.out.println("table of"+" "+ num +":");
 	for(int i = 1; i <= 10 ; i++) {
 		
 		int result = num * i;
+		
 		System.out.println(result);
 		
 	}
