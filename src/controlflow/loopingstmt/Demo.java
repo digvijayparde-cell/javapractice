77b86f6e63ab202ed5b8f880279cf6cf8d94c386
digvijay parade <digvijayparde@gmail.com>
2026-07-08 12:56:44 +0530

    idk im just coding whatever comes to mind

--- src/controlflow/loopingstmt/Demo.java
+++ src/controlflow/loopingstmt/Demo.java
@@ -2,23 +2,28 @@ package controlflow.loopingstmt;
 
 public class Demo {
 
-	int dash ;
+	static int dash ;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		mg();
+		mg(17);
 		
 
 	}
-	public static void mg() {
+	public static void mg(int x) {
 		
-		Demo obj = new Demo();
-		for(int i = obj.dash = 1;i <= 100; i++ ){
+		
+		for(int i = dash = 1;i <= 100; i++, dash++, x++){
 			
 			System.out.println("HELP!!");
+			System.out.println(dash);
+			System.out.println(x);
+			
+			
 			
 		}
-		
+		System.out.println(dash);
+		System.out.println(x);
 	}
 
 
