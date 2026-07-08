871e351d1aba304870da585601e41d585732c192
digvijay parade <digvijayparde@gmail.com>
2026-07-08 12:49:01 +0530

    ;;

---
+++ src/controlflow/loopingstmt/Demo.java
@@ -0,0 +1,25 @@
+package controlflow.loopingstmt;
+
+public class Demo {
+
+	int dash ;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		mg();
+		
+
+	}
+	public static void mg() {
+		
+		Demo obj = new Demo();
+		for(int i = obj.dash = 1;i <= 100; i++ ){
+			
+			System.out.println("HELP!!");
+			
+		}
+		
+	}
+
+
+}
