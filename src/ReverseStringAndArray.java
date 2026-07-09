eb34407acc3900bed81b015520a7436f9247e23b
digvijay parade <digvijayparde@gmail.com>
2026-07-09 16:18:32 +0530

    ..

--- src/ReverseStringAndArray.java
+++ src/ReverseStringAndArray.java
@@ -1,20 +1,42 @@
+public class ReverseStringAndArray {
 
-public class StringReverse {
-	
-	static String reversed ="";
-public static void main(String[]args ){
-	
-	reve("VIjay");
-	
-}
-
-public static void reve(String name) {
-	
-	for (int i = name.length()- 1 ; i >= 0 ; i--) {
+	public static void main(String[] args) {
 		
-		reversed = reversed + name.charAt(i);
+		String[] names = {"nanu", "Gannu", "sonu", "monu"};
+		
+		reve("VIjay");
+		reveArray(names);
+		
 	}
-	System.out.println(reversed);
-}
+
+	public static void reve(String name) {
+		
+		String reversed = "";  
+		
+		for (int i = name.length() - 1; i >= 0; i--) {
+			reversed = reversed + name.charAt(i);
+		}
+		
+		System.out.println(reversed);
+	}
 	
+	public static void reveArray(String[] names) {
+		
+		int start = 0;
+		int end = names.length - 1;
+
+		while(start < end) {
+			String temp = names[start];
+			names[start] = names[end];
+			names[end] = temp;
+			
+			start++;
+			end--;
+		}
+
+		for(int i = 0; i < names.length; i++) {
+			System.out.print(names[i] + " ");
+		}
+		
+	}
 }
