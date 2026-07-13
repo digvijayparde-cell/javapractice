c53466ef0dc1dcc99a0f27a876a5702b74e0415f
digvijay parade <digvijayparde@gmail.com>
2026-07-13 23:56:52 +0530

    //

---
+++ src/scannerTopic/Message.java
@@ -0,0 +1,25 @@
+package scannerTopic;
+
+import java.util.Scanner;
+
+public class Message {
+
+	public static void main(String[]args) {
+		
+		msg();
+		
+	}
+	public static void msg() {
+		
+		String greet;
+		String intro;
+		Scanner sc= new Scanner(System.in);
+		
+		greet = sc.nextLine();
+		intro = sc.nextLine();
+		
+		System.out.println();
+		
+		
+	}
+}
