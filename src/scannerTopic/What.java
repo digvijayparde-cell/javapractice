c772d34e16fb7ea03a9c0b33f71623031a486573
digvijay parade <digvijayparde@gmail.com>
2026-07-13 12:56:29 +0530

    what!

---
+++ src/scannerTopic/What.java
@@ -0,0 +1,24 @@
+package scannerTopic;
+
+import java.util.Scanner;
+
+public class What{
+	
+	public static void main(String[]argd) {
+
+		w();
+		
+}
+	
+	public static void w() {
+		
+		String wha;
+		Scanner sc = new Scanner(System.in);
+		for(int i = 1; i <= 1000;i++) {
+		wha = sc.nextLine();
+		System.out.println(wha);
+		
+		}
+		
+	}
+}
