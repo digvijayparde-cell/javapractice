2b563179974e5a279f196f62a30a7c11d11f9af9
digvijay parade <digvijayparde@gmail.com>
2026-07-18 22:56:10 +0530

    ;

---
+++ src/Halfofinteger.java
@@ -0,0 +1,26 @@
+
+import java. util.*;
+
+public class Halfofinteger {
+    public static void main(String [] args){
+
+
+
+    Scanner sc = new Scanner(System.in)  ;
+
+    int number = sc.nextInt();
+
+
+    int halfnumber = (int) (number / 2.0);
+
+
+    System.out.println(halfnumber);
+
+    }
+
+
+
+
+
+
+}
