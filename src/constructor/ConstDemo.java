975b3699f2e04962f680d6f55476931a4db33f94
digvijay parade <digvijayparde@gmail.com>
2026-07-19 17:54:56 +0530

    //

--- src/constructor/ConstDemo.java
+++ src/constructor/ConstDemo.java
@@ -1,5 +1,16 @@
 package constructor;
 
 public class ConstDemo {
+	
+	public ConstDemo() {
+		
+		System.out.println("this is constructor");
+	}
+	public static void main(String[]args) {
+		
+		System.out.println("this is mainmethod");
+		
+		ConstDemo obj = new ConstDemo();
+	}
 
 }
