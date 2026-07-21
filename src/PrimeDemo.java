cbf19a0cc04b208070651e8d59db9ae3a38e67a3
digvijay parade <digvijayparde@gmail.com>
2026-07-21 18:08:35 +0530

    ''

---
+++ src/PrimeDemo.java
@@ -0,0 +1,28 @@
+
+
+public class PrimeDemo {
+
+	public static void main(String[] args) {
+		primeNum();
+	}
+
+	public static void primeNum() {
+
+		for(int i = 2; i <= 1000; i++) {
+
+			boolean isPrime = true;
+			for(int j = 2; j < i; j++) {
+
+				if (i % j == 0) {
+					isPrime = false;
+					break;
+				}
+			} 
+
+			if(isPrime) {
+				System.out.println(i);
+			}
+
+		}
+	}
+}
