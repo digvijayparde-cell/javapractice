6c02f69073070089013421dec958c8ff3aeeb5cf
digvijay parade <digvijayparde@gmail.com>
2026-07-22 14:39:00 +0530

    //

---
+++ src/array/ArrayD.java
@@ -0,0 +1,46 @@
+package array;
+
+import java.util.*;
+public class ArrayD {
+
+	public static void main(String[]args) {
+		
+		ArrayD obj = new ArrayD();
+		
+		obj.fixAr().dynamicAr();
+		
+	}
+	ArrayD fixAr() {
+		
+		Scanner sc = new Scanner(System.in);
+		int [] fix = new int[5];
+		
+		for(int i = 0;i< fix.length ;i++) {
+			
+			fix[i] = sc.nextInt();
+		}
+		System.out.println(Arrays.toString(fix));
+		
+		return this;
+	}
+	ArrayD dynamicAr() {
+		
+		ArrayList <Integer> numbers = new ArrayList<>();
+		
+		Scanner scan = new Scanner(System.in);
+		numbers.add(scan.nextInt());
+		numbers.add(scan.nextInt());
+		numbers.add(scan.nextInt());
+		numbers.add(scan.nextInt());
+		numbers.add(scan.nextInt());
+		numbers.add(scan.nextInt());
+		numbers.add(scan.nextInt());
+		numbers.add(scan.nextInt());
+		numbers.add(scan.nextInt());
+		numbers.add(scan.nextInt());
+		
+		System.out.println(numbers);
+		return this;
+		
+	}
+}
